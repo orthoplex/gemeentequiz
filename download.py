028fc556b577d0770f9e2beee105fc247f1eb63d
orthoplex <inf18117@lehre.dhbw-stuttgart.de>
2022-01-23 21:31:08 +0100

    make it somewhat playable

--- download.py
+++ download.py
@@ -3,30 +3,33 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-xml = requests.get('https://www.gemeentenatlas.nl/', auth=('user', 'pass')).text
+xml = requests.get('https://www.gemeentenatlas.nl/').text
 soup = BeautifulSoup(xml, "lxml")
 
 areas = dict()
 
-for e in soup.find_all("area"):
-    title = e["title"]
-    title = title.replace("%20", " ")
-    title = re.sub(r"(?<=[a-z])(?=[A-Z])", "-", title)
-    if title == "Korendijk":
-        title = "Hoeksche Waard"
-    if title == "Bussum":
-        title = "Gooise Meren"
-    if not title in areas:
-        areas[title] = []
-    area = " ".join(e["coords"].split(", "))
-    areas[title].append(area)
+for match in soup.find_all("area"):
+    name = match["title"]
+
+    # clean-up and corrections
+    name = name.replace("%20", " ")
+    name = re.sub(r"(?<=[a-z])(?=[A-Z])", "-", name)
+    if name == "Korendijk":
+        name = "Hoeksche Waard"
+    if name == "Bussum":
+        name = "Gooise Meren"
+    if not name in areas:
+        areas[name] = []
+
+    area = " ".join(match["coords"].split(", "))
+    areas[name].append(area)
 
 with open("map.js", "w") as outfile:
     w = outfile.write
     w("areas={")
-    for e in sorted(areas):
-        w(f'"{e}":[')
-        for area in areas[e]:
+    for name in sorted(areas):
+        w(f'"{name}":[')
+        for area in areas[name]:
             w(f"'{area}',")
         w("],")
     w("};")
